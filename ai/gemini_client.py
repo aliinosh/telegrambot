f58b75bd5f583818a131b794a93f8d597fdce153
aliinosh <muhammadaliazimzonov@gmail.com>
2026-03-12 21:50:14 +0600

    Update gemini_client.py

--- ai/gemini_client.py
+++ ai/gemini_client.py
@@ -1,1 +1,12 @@
+import google.generativeai as genai
+from config.settings import GEMINI_API_KEY
 
+genai.configure(api_key=GEMINI_API_KEY)
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+def generate(prompt):
+
+    response = model.generate_content(prompt)
+
+    return response.text
