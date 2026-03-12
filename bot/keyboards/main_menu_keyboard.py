52489a59aef29cb8e897504de3bd3602fbab5170
aliinosh <muhammadaliazimzonov@gmail.com>
2026-03-12 22:04:11 +0600

    Update main_menu_keyboard.py

--- bot/keyboards/main_menu_keyboard.py
+++ bot/keyboards/main_menu_keyboard.py
@@ -1,1 +1,24 @@
+from aiogram.types import ReplyKeyboardMarkup
+from aiogram.types import KeyboardButton
 
+
+def main_menu_keyboard():
+
+    keyboard = [
+
+        [
+            KeyboardButton(text="📊 Presentation"),
+            KeyboardButton(text="📄 Assignment")
+        ],
+
+        [
+            KeyboardButton(text="🌐 Web App"),
+            KeyboardButton(text="⚙ Settings")
+        ]
+
+    ]
+
+    return ReplyKeyboardMarkup(
+        keyboard=keyboard,
+        resize_keyboard=True
+    )
