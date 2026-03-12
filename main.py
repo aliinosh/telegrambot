8bb884ece220e8cb88e84f745acd114b9f5ebfdc
aliinosh <muhammadaliazimzonov@gmail.com>
2026-03-12 22:14:42 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from bot.handlers.start_handler import router as start_router
+from bot.handlers.language_handler import router as language_router
+from bot.handlers.presentation_handler import router as presentation_router
+from bot.handlers.assignment_handler import router as assignment_router
+from bot.handlers.settings_handler import router as settings_router
+from bot.handlers.webapp_handler import router as webapp_router
 
+
+dp.include_router(start_router)
+dp.include_router(language_router)
+dp.include_router(presentation_router)
+dp.include_router(assignment_router)
+dp.include_router(settings_router)
+dp.include_router(webapp_router)
