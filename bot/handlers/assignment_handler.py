3a2e40efd1be654513073222876ee796efcce3f0
aliinosh <muhammadaliazimzonov@gmail.com>
2026-03-12 22:06:01 +0600

    Update assignment_handler.py

--- bot/handlers/assignment_handler.py
+++ bot/handlers/assignment_handler.py
@@ -1,1 +1,23 @@
+from aiogram import Router
+from aiogram.types import Message
+from services.assignment_service import create_assignment
 
+router = Router()
+
+
+@router.message(lambda m: m.text == "📄 Assignment")
+async def assignment(message: Message):
+
+    await message.answer("Send assignment topic")
+
+
+@router.message()
+async def generate_assignment(message: Message):
+
+    topic = message.text
+
+    path = create_assignment(topic, 3)
+
+    await message.answer_document(
+        document=open(path, "rb")
+    )
