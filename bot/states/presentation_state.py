7c86c81be35b8453fbe9d63edd0212dd8f7ea844
aliinosh <muhammadaliazimzonov@gmail.com>
2026-03-12 22:04:51 +0600

    Update presentation_state.py

--- bot/states/presentation_state.py
+++ bot/states/presentation_state.py
@@ -1,1 +1,11 @@
+from aiogram.fsm.state import StatesGroup
+from aiogram.fsm.state import State
 
+
+class PresentationState(StatesGroup):
+
+    topic = State()
+
+    slide_count = State()
+
+    images = State()
