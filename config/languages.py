d0bcf5873953a7262f40579e058cc11087820e4d
aliinosh <muhammadaliazimzonov@gmail.com>
2026-03-12 21:47:47 +0600

    Update languages.py

--- config/languages.py
+++ config/languages.py
@@ -1,1 +1,24 @@
+LANGUAGES = {
 
+    "uz": {
+        "start": "Tilni tanlang",
+        "presentation": "📊 Taqdimot yaratish",
+        "assignment": "📄 Mustaqil ish",
+        "settings": "⚙ Sozlamalar"
+    },
+
+    "ru": {
+        "start": "Выберите язык",
+        "presentation": "📊 Создать презентацию",
+        "assignment": "📄 Самостоятельная работа",
+        "settings": "⚙ Настройки"
+    },
+
+    "en": {
+        "start": "Choose language",
+        "presentation": "📊 Create Presentation",
+        "assignment": "📄 Assignment",
+        "settings": "⚙ Settings"
+    }
+
+}
