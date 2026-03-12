412a669a11fe86ce4ebcbde97d354a8cf0266256
aliinosh <muhammadaliazimzonov@gmail.com>
2026-03-12 22:19:27 +0600

    Update healthcheck.py

--- server/healthcheck.py
+++ server/healthcheck.py
@@ -1,1 +1,13 @@
+from aiohttp import web
 
+async def health(request):
+
+    return web.Response(text="Bot is running")
+
+app = web.Application()
+
+app.router.add_get("/health", health)
+
+if __name__ == "__main__":
+
+    web.run_app(app)
