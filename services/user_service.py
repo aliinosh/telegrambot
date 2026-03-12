1d0f4871081c3b65dda31b36787a7745e86ccaf4
aliinosh <muhammadaliazimzonov@gmail.com>
2026-03-12 21:58:23 +0600

    Update user_service.py

--- services/user_service.py
+++ services/user_service.py
@@ -1,1 +1,25 @@
+users = {}
 
+
+def create_user(user_id):
+
+    if user_id not in users:
+
+        users[user_id] = {
+            "language": "uz"
+        }
+
+
+def set_language(user_id, language):
+
+    if user_id in users:
+
+        users[user_id]["language"] = language
+
+
+def get_language(user_id):
+
+    if user_id in users:
+        return users[user_id]["language"]
+
+    return "uz"
