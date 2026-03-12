d80f9a665da6a2d248ccdf729c35760cd8831087
aliinosh <muhammadaliazimzonov@gmail.com>
2026-03-12 22:16:59 +0600

    Update app.js

--- webapp/app.js
+++ webapp/app.js
@@ -1,1 +1,24 @@
+let tg = window.Telegram.WebApp
 
+tg.expand()
+
+function generate(){
+
+let topic = document.getElementById("topic").value
+
+let slides = document.getElementById("slides").value
+
+let images = document.getElementById("images").checked
+
+
+let data = {
+
+topic: topic,
+slides: slides,
+images: images
+
+}
+
+tg.sendData(JSON.stringify(data))
+
+}
