3c94f1c2ed0f66edbb1a5b70587e2ad7d9e0b0fb
aliinosh <muhammadaliazimzonov@gmail.com>
2026-03-12 22:00:05 +0600

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,1 +1,46 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+from database.db import Base
 
+
+class User(Base):
+
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    telegram_id = Column(String, unique=True)
+
+    language = Column(String, default="uz")
+
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+
+class Presentation(Base):
+
+    __tablename__ = "presentations"
+
+    id = Column(Integer, primary_key=True)
+
+    user_id = Column(String)
+
+    topic = Column(String)
+
+    file_path = Column(String)
+
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+
+class Assignment(Base):
+
+    __tablename__ = "assignments"
+
+    id = Column(Integer, primary_key=True)
+
+    user_id = Column(String)
+
+    topic = Column(String)
+
+    file_path = Column(String)
+
+    created_at = Column(DateTime, default=datetime.utcnow)
