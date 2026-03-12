b708f40fffbb9ae9e13505ead0913281601198c6
aliinosh <muhammadaliazimzonov@gmail.com>
2026-03-12 22:05:22 +0600

    Update start_handler.py

--- bot/handlers/start_handler.py
+++ bot/handlers/start_handler.py
@@ -1,1 +1,17 @@
+from aiogram import Router
+from aiogram.types import Message
 
+from bot.keyboards.main_menu_keyboard import main_menu_keyboard
+
+router = Router()
+
+
+@router.message()
+async def start_menu(message: Message):
+
+    if message.text == "menu":
+
+        await message.answer(
+            "Main Menu",
+            reply_markup=main_menu_keyboard()
+        )
