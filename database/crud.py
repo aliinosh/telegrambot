cbfaab3f41a655b0370d3299dba660b79f2e56c4
aliinosh <muhammadaliazimzonov@gmail.com>
2026-03-12 22:00:31 +0600

    Update crud.py

--- database/crud.py
+++ database/crud.py
@@ -1,1 +1,45 @@
+from database.models import User, Presentation, Assignment
+from database.db import SessionLocal
 
+
+def create_user(telegram_id):
+
+    db = SessionLocal()
+
+    user = User(telegram_id=telegram_id)
+
+    db.add(user)
+
+    db.commit()
+
+    db.close()
+
+
+def get_user(telegram_id):
+
+    db = SessionLocal()
+
+    user = db.query(User).filter(
+        User.telegram_id == telegram_id
+    ).first()
+
+    db.close()
+
+    return user
+
+
+def set_language(telegram_id, language):
+
+    db = SessionLocal()
+
+    user = db.query(User).filter(
+        User.telegram_id == telegram_id
+    ).first()
+
+    if user:
+
+        user.language = language
+
+        db.commit()
+
+    db.close()
