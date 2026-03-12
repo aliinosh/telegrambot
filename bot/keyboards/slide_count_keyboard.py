ec5ac83db9e7dccf3017f28cd79e3393d7ab3a11
aliinosh <muhammadaliazimzonov@gmail.com>
2026-03-12 22:09:08 +0600

    Update slide_count_keyboard.py

--- bot/keyboards/slide_count_keyboard.py
+++ bot/keyboards/slide_count_keyboard.py
@@ -1,1 +1,24 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+
+def slide_count_keyboard():
+
+    keyboard = [
+
+        [
+            KeyboardButton(text="5"),
+            KeyboardButton(text="10"),
+            KeyboardButton(text="15")
+        ],
+
+        [
+            KeyboardButton(text="20"),
+            KeyboardButton(text="25")
+        ]
+
+    ]
+
+    return ReplyKeyboardMarkup(
+        keyboard=keyboard,
+        resize_keyboard=True
+    )
