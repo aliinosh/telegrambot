51cca19110dc833d48d29fb5333c06752c671b2b
aliinosh <muhammadaliazimzonov@gmail.com>
2026-03-12 22:02:57 +0600

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,1 +1,9 @@
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from config.settings import BOT_TOKEN
 
+bot = Bot(token=BOT_TOKEN)
+
+storage = MemoryStorage()
+
+dp = Dispatcher(storage=storage)
