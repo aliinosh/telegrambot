395eb35d3730ba84306987007d33c39ee635af83
aliinosh <muhammadaliazimzonov@gmail.com>
2026-03-12 22:03:49 +0600

    Update language_keyboard.py

--- bot/keyboards/language_keyboard.py
+++ bot/keyboards/language_keyboard.py
@@ -1,1 +1,20 @@
+from aiogram.types import ReplyKeyboardMarkup
+from aiogram.types import KeyboardButton
 
+
+def language_keyboard():
+
+    keyboard = [
+
+        [
+            KeyboardButton(text="🇺🇿 O'zbek"),
+            KeyboardButton(text="🇷🇺 Русский"),
+            KeyboardButton(text="🇬🇧 English")
+        ]
+
+    ]
+
+    return ReplyKeyboardMarkup(
+        keyboard=keyboard,
+        resize_keyboard=True
+    )
