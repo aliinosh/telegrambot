f404a0ceb94ab9d1445a9abc4b0b15d59b019d52
aliinosh <muhammadaliazimzonov@gmail.com>
2026-03-12 22:03:19 +0600

    Update commands.py

--- bot/commands.py
+++ bot/commands.py
@@ -1,1 +1,16 @@
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
+from bot.keyboards.language_keyboard import language_keyboard
+
+router = Router()
+
+
+@router.message(CommandStart())
+async def start(message: Message):
+
+    await message.answer(
+        "Tilni tanlang / Choose language",
+        reply_markup=language_keyboard()
+    )
